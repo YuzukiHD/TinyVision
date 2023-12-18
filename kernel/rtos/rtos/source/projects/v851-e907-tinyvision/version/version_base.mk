50b1b8810845481ca3107476beed9041b897f82d
YuzukiTsuru <gloomyghost@gloomyghost.com>
2023-12-18 13:28:08 +0800

    [e907] support e907 tinyvision

---
+++ kernel/rtos/rtos/source/projects/v851-e907-tinyvision/version/version_base.mk
@@ -0,0 +1,10 @@
+# define the verions of the image
+# format: main
+# such as 1, 2
+# NOTICE: the range of main version is from 0 to 31,
+# ROOT_ROLLBACK_USE ---0:not used,1:used
+# when you change the version, you must increase main version, and never reduce the versions.
+# the default version is 0
+
+ROOT_ROLLBACK_USED = 1
+MAIN_VERSION = 0
